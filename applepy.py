5a4f2237b5adaca77d935e07af62f511ada805ea
cemyr <socsirius@gmail.com>
2019-08-10 20:23:34 +0200

    only included an attempt to import inspection and some comments, and one more file as a quiet notebook...

--- applepy.py
+++ applepy.py
@@ -2,5 +2,20 @@
 import pathlib
 from pathlib import Path
 from matplotlib import pyplot
+print("hi this is a welcome message for you")
+
+#I hope I soon come up with some meaningful stuff to code
+#as this is just to test and train git
+#however, doesnt really matter, as long as I can write
+#boring spontaneous comments
+import inspection
+#dont even know if inspection exisits or has to be pip installed ..lets find out
+#another thought...should convert this thang into jupyter to get it flying ...how will git
+#be integitrated ...hihi
+#well...we'll see
+# nice cool exercise on the side is to learn more and be "cool with " vim
+# dont really know if its going to be my fav editor, will see that later
+# this is reserverd to be uncommented (the following line) in jupy
+# %who
 x = input("whats your name?")
 print("hi, how are you", " ", x)
---
+++ note1.py
@@ -0,0 +1,4 @@
+#as the name says...just a small "quiet" notebook
+#nothing in it but comments on what I do here...like a log
+# will now merge the master into the branch and then continue ont he explorer branch
+#dont mind the typos doesnt really matter right now
